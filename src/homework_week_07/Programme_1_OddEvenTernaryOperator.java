196c1c09800b2431cd1e17ffd5e5f41cc6b3bc36
Krp8592 <patelkashyap8555@gmail.com>
2023-03-30 23:46:56 +0100

    Commit

--- src/homework_week_07/Programme_1_OddEvenTernaryOperator.java
+++ src/homework_week_07/Programme_1_OddEvenTernaryOperator.java
@@ -1,2 +1,28 @@
-package homework_week_07;public class Programme_1_OddEvenTernaryOperator {
+package homework_week_07;
+
+import java.util.Scanner;
+
+/**
+ * 1. Write a java program that tells us that input is odd or even ?
+ * Hint : Use ternary operator(? :)
+ */
+
+public class Programme_1_OddEvenTernaryOperator {
+
+    public static void main(String[] args) {
+        //Scanner declaration for reading input from console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number : ");
+        int number = scanner.nextInt();
+        isItOddOrEvenNumber(number);
+
+        scanner.close();
+    }
+
+    //Checking the number is odd or even
+    public static void isItOddOrEvenNumber(int number) {
+        //ternary operator is used
+        String evenOdd = (number % 2 == 0) ? "Even" : "Odd";
+        System.out.println("The " + number + " is " + evenOdd + " number");
+    }
 }
--- src/homework_week_07/Programme_9_printCityNameWithSwitch.java
+++ src/homework_week_07/Programme_9_printCityNameWithSwitch.java
@@ -1,2 +1,46 @@
-package homework_week_07;public class Programme_9_printCityNameWithSwitch {
+package homework_week_07;
+
+import java.util.Scanner;
+
+/**
+ * Input any alphabet from "A to F" and print their city name accordingly (use if else); if
+ * any other alphabet should be invalid entry USING SWITCH STATEMENT.
+ */
+
+public class Programme_9_printCityNameWithSwitch {
+    public static void main(String[] args) {
+        //Scanner declaration for reading input from console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter alphabet between A and F : ");
+        String city = scanner.next().toUpperCase();
+        Programme_9_printCityNameWithSwitch cityName = new Programme_9_printCityNameWithSwitch();
+        cityName.printCityName(city);
+        //Closing the scanner object
+        scanner.close();
+
+    }
+
+    //Printing city name
+    public void printCityName(String city) {
+        switch (city) {
+            case "A":
+                System.out.println("Aberdeen");
+                break;
+            case "B":
+                System.out.println("Belfast");
+                break;
+            case "C":
+                System.out.println("Cambridge");
+                break;
+            case "D":
+                System.out.println("Derby");
+            case "E":
+                System.out.println("Edinburgh");
+            case "F":
+                System.out.println("Feltham");
+            default:
+                System.out.println("The alphabet you entered if not between A and F");
+        }
+
+    }
 }
